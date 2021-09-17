374d312f96098134859feffafd73d1eb70f39f57
Olivier Gamache <olivier@glo.quebec>
2021-09-17 11:49:58 -0400

    ↩️  moving some old config in here

---
+++ karabiner/README.md
@@ -0,0 +1,7 @@
+# karabiner - cool keyrebinder for macos
+
+## Requirements
+
+You will need:
+* Karabiner-Elements
+* Karabiner-EventViewer
--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -18,34 +18,80 @@ exit; fi
 
 export DOTFILES=~/.dotfiles
 
+###############################
+#                             #
+#     Utilitary functions     #
+#                             #
+###############################
+
 # Logging stuff.
 function e_header()   { echo -e "\n\033[1m$@\033[0m"; }
 function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
 function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
 function e_arrow()    { echo -e " \033[1;34m➜\033[0m  $@"; }
 
+DEFAULT_SpinnerFrames=('_' '_' '_' '-' '`' '`' "'" '´' '-' '_' '_' '_')
+function spinner() {
+  local frameRef
+  local action="${1}"
+  local label="${2} "
+  local spinnerRef="${3-DEFAULT_SpinnerFrames}"
+  local spinnerFrames=$(eval "echo \${!${spinnerRef}[@]}")
 
+  spinnerRun() {
+    while true; do
+      for frame in ${spinnerFrames[@]}; do
+        frameRef="${spinnerRef}[${frame}]"
+        echo "${label}${!frameRef}"
+        tput cuu1 tput el
+        sleep 0.05
+      done
+    done
+    echo -e "\r"
+  }
+
+  spinnerRun &
+  local spinnerPid=$!
+  ${action} 1>/dev/null 2>/dev/null
+  kill "${spinnerPid}"
+  wait "${spinnerPid}" 2>/dev/null
+}
+
+function symlink() {
+  local config="${1}"
+  ln -s $(pwd)/$config ~/.config/
+}
+
+
+###############################
 
 # Initialize
 if [[ ! -d $DOTFILES ]]; then
-  e_header "Downloading dotfiles..."
-  git clone git@github.com:$GITHUB_REPOSITORY.git $DOTFILES
+  spinner "git clone git@github.com:$GITHUB_REPOSITORY.git $DOTFILES" "Loading dotfiles..."
   cd $DOTFILES
 fi
 
 e_header "Setuping nvim..."
-ln -s $(pwd)/nvim ~/.config/
+symlink nvim
 e_success "Nvim: done!"
 
 e_header "Setting up git..."
-ln -s $(pwd)/gitconfig ~/.gitconfig
-echo -n "What is your name?"
+symlink git
+echo -n "What is your name? "
 read name
-git config --global user.name "$name"
+#git config --global user.name "$name"
 
-echo -n "What is your email?"
+echo -n "What is your email? "
 read email
-git config --global user.email "$email"
-e_success "Git: gone!"
+#git config --global user.email "$email"
+e_success "Git: done!"
+
+e_header "Setuping karabiner..."
+symlink karabiner
+e_success "Karabiner: done!"
+
+e_header "Setuping iterm2..."
+symlink iterm2
+e_success "iterm2: done!"
 
 e_header "All done!"
